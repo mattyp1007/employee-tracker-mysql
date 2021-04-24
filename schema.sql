741a8c799a9b5952e1efc7c4ad84dbc3d6fe0716
Matthew Parker <matt.parker1007@gmail.com>
2021-04-24 12:39:40 -0500

    created sql schema

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+	id INT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+	id INT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+	id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
